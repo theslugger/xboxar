4c2b523b6bc38b7a7bdbb09e52a9a6dda5596fdb
Wei Liu <48640089+theslugger@users.noreply.github.com>
2024-08-17 22:08:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// 从持久化存储读取第一次存储的ID
+// 尝试读取先前存储的ID
 let firstStoredIds = $persistentStore.read("idsToReplace");
-
-// 从持久化存储读取第二次存储的ID
-let secondStoredIds = $persistentStore.read("idsToTarget");
 
 // 检查是否存在响应体
 if ($response.body) {
@@ -17,19 +14,19 @@ if ($response.body) {
         };
 
         if (!firstStoredIds) {
-            // 如果第一次的ID尚未存储，存储这些ID为第一次的ID
+            // 第一次存储ID
             $persistentStore.write(JSON.stringify(ids), "idsToReplace");
             console.log("First set of IDs stored: " + JSON.stringify(ids));
             $notification.post("第一次ID存储", "成功存储以下ID为待替换ID:", `产品ID: ${ids.productId}, 可用性ID: ${ids.availabilityId}, SKU ID: ${ids.skuId}`);
-        } else if (!secondStoredIds) {
-            // 如果第一次的ID已经存储，而第二次的ID尚未存储，存储这些ID为第二次的ID
+        } else {
+            // 已经有第一次存储的ID，存储第二次捕获的ID到另一个变量
             $persistentStore.write(JSON.stringify(ids), "idsToTarget");
             console.log("Second set of IDs stored: " + JSON.stringify(ids));
             $notification.post("第二次ID存储", "成功存储以下ID为目标替换ID:", `产品ID: ${ids.productId}, 可用性ID: ${ids.availabilityId}, SKU ID: ${ids.skuId}`);
-        } else {
-            // 如果第一次和第二次的ID都已经存储，可能需要处理额外的逻辑或忽略此次操作
-            console.log("Both sets of IDs are already stored. Ignoring this capture.");
         }
+    } else {
+        console.log("No line items found in the cart.");
+        $notification.post("购物车数据错误", "购物车中没有找到条目", "");
     }
     $done({});
 } else {
